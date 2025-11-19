441c1edf1ce3611f78d1b343efc981c16364baaa
구현모 <funrace252@gmail.com>
2025-11-19 15:20:13 +0900

    [FEAT] WatchOS 타겟 및 기본 UI 구성 추가

---
+++ Mentory/MentoryWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MentoryWatch Watch App
+//
+//  Created by 구현모 on 11/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mentory/MentoryWatch Watch App/MentoryWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentoryWatchApp.swift
+//  MentoryWatch Watch App
+//
+//  Created by 구현모 on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MentoryWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
