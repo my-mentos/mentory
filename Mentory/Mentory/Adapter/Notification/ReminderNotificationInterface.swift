4bb9e88253864d4d66e12f84bcef923d27afbb80
kmw4561@gmail.com <kmw4561@gmail.com>
2025-12-23 13:30:21 +0900

    [refactor] MentoryDB 프레임워크 타겟 제거

--- Mentory/Mentory/Adapter/Notification/ReminderNotificationInterface.swift
+++ Mentory/Mentory/Adapter/Notification/ReminderNotificationInterface.swift
@@ -1,14 +1,14 @@
+////
+////  ReminderNotificationInterface.swift
+////  Mentory
+////
+////  Created by 구현모 on 12/04/25.
+////
 //
-//  ReminderNotificationInterface.swift
-//  Mentory
+//import Foundation
 //
-//  Created by 구현모 on 12/04/25.
-//
-
-import Foundation
-
-protocol ReminderNotificationInterface: Sendable {
-    func requestAuthorizationIfNeeded() async
-    func scheduleWeeklyReminder(baseDate: Date, reminderTime: Date) async
-    func cancelAllWeeklyReminders() async
-}
+//protocol ReminderNotificationInterface: Sendable {
+//    func requestAuthorizationIfNeeded() async
+//    func scheduleWeeklyReminder(baseDate: Date, reminderTime: Date) async
+//    func cancelAllWeeklyReminders() async
+//}
